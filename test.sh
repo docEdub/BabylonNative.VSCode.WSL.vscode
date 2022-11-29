48cef0692bcd212675d0a07c00ba2a0313cd9945
Andy Fillebrown <afillebrown@microsoft.com>
2022-11-29 17:08:50 -0500

    Add 15 second timeout to example script for stress testing setTimeout

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
 
-for i in {1..100}; do timeout -k 1 1.5 echo $i && ./UnitTests; done | tee ./UnitTests.log
+for i in {1..1000}; do timeout -k 1 1.5 echo $i && timeout 15 ./UnitTests; done | tee ./UnitTests.log
