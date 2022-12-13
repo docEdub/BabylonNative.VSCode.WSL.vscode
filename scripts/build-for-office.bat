b3fd081bf702513ba4c726a2c6b826d571136c0a
Andy Fillebrown <afillebrown@microsoft.com>
2022-12-12 23:47:08 -0500

    Update build-for-office.bat with correct JS engine and .js file copying

--- scripts/build-for-office.bat
+++ scripts/build-for-office.bat
@@ -1,73 +1,71 @@
-cd ..\..
+pushd ..\..
 
-if exist .\.install\ (rmdir /s /q .\.install)
-
-
-
-if exist .\.build\Win32_x64_Chakra_D3D11\Debug\ (rmdir /s /q .\.build\Win32_x64_Chakra_D3D11\Debug)
-cmake -B .\.build\Win32_x64_Chakra_D3D11\Debug -A x64 -D NAPI_JAVASCRIPT_ENGINE=Chakra -D GRAPHICS_API=D3D11 -D CMAKE_CONFIGURATION_TYPES=Debug -D CMAKE_INSTALL_PREFIX=.\.install\x64\Debug
-cmake --build .\.build\Win32_x64_Chakra_D3D11\Debug --config Debug --parallel 20
-cmake --build .\.build\Win32_x64_Chakra_D3D11\Debug --config Debug --target INSTALL
-
-if exist .\.build\Win32_x64_Chakra_D3D11\Release\ (rmdir /s /q .\.build\Win32_x64_Chakra_D3D11\Release)
-cmake -B .\.build\Win32_x64_Chakra_D3D11\Release -A x64 -D NAPI_JAVASCRIPT_ENGINE=Chakra -D GRAPHICS_API=D3D11 -D CMAKE_CONFIGURATION_TYPES=RelWithDebInfo -D CMAKE_INSTALL_PREFIX=.\.install\x64\Release
-cmake --build .\.build\Win32_x64_Chakra_D3D11\Release --config RelWithDebInfo --parallel 20
-cmake --build .\.build\Win32_x64_Chakra_D3D11\Release --config RelWithDebInfo --target INSTALL
+if exist .\.install\ (
+    rmdir /s /q .\.install
+)
 
 
+pushd Apps
+start /wait cmd /c npm install
+popd ..
 
-@REM The x86 builds are not needed for Office development right now.
+if exist .\.build\Win32_x64_Napi-c-external_D3D11\Debug\ (
+    rmdir /s /q .\.build\Win32_x64_Napi-c-external_D3D11\Debug
+)
 
-@REM if exist .\.build\Win32_x86_Chakra_D3D11\Debug\ (rmdir /s /q .\.build\Win32_x86_Chakra_D3D11\Debug)
-@REM cmake -B .\.build\Win32_x86_Chakra_D3D11\Debug -A Win32 -D NAPI_JAVASCRIPT_ENGINE=Chakra -D GRAPHICS_API=D3D11 -D CMAKE_CONFIGURATION_TYPES=Debug -D CMAKE_INSTALL_PREFIX=.\.install\Win32\Debug
-@REM cmake --build .\.build\Win32_x86_Chakra_D3D11\Debug --config Debug --parallel 20
-@REM cmake --build .\.build\Win32_x86_Chakra_D3D11\Debug --config Debug --target INSTALL
+@REM v142 == Visual Studio 2019 toolchain.
+@REM v143 == Visual Studio 2022 toolchain.
+cmake -B .\.build\Win32_x64_Napi-c-external_D3D11\Debug -A x64 -T "v142" ^
+    -DNAPI_JAVASCRIPT_ENGINE=Napi-c-external ^
+    -DGRAPHICS_API=D3D11 ^
+    -DCMAKE_CONFIGURATION_TYPES=Debug ^
+    -DBABYLON_NATIVE_BUILD_APPS=Off ^
+    -DCMAKE_INSTALL_PREFIX=.\.install\x64\Debug
+if  %ERRORLEVEL% NEQ 0 (goto error)
+cmake --build .\.build\Win32_x64_Napi-c-external_D3D11\Debug --config Debug --parallel 20
+if  %ERRORLEVEL% NEQ 0 (goto error)
+cmake --build .\.build\Win32_x64_Napi-c-external_D3D11\Debug --config Debug --target INSTALL
+if  %ERRORLEVEL% NEQ 0 (goto error)
 
-@REM if exist .\.build\Win32_x86_Chakra_D3D11\Release\ (rmdir /s /q .\.build\Win32_x86_Chakra_D3D11\Release)
-@REM cmake -B .\.build\Win32_x86_Chakra_D3D11\Release -A Win32 -D NAPI_JAVASCRIPT_ENGINE=Chakra -D GRAPHICS_API=D3D11 -D CMAKE_CONFIGURATION_TYPES=RelWithDebInfo -D CMAKE_INSTALL_PREFIX=.\.install\Win32\Release
-@REM cmake --build .\.build\Win32_x86_Chakra_D3D11\Release --config RelWithDebInfo --parallel 20
-@REM cmake --build .\.build\Win32_x86_Chakra_D3D11\Release --config RelWithDebInfo --target INSTALL
+@REM if exist .\.build\Win32_x64_Napi-c-external_D3D11\Release\ (
+    @REM rmdir /s /q .\.build\Win32_x64_Napi-c-external_D3D11\Release
+@REM )
+@REM cmake -B .\.build\Win32_x64_Napi-c-external_D3D11\Release -A x64 -T "v142" ^
+    @REM -DNAPI_JAVASCRIPT_ENGINE=Napi-c-external ^
+    @REM -DGRAPHICS_API=D3D11 ^
+    @REM -DCMAKE_CONFIGURATION_TYPES=RelWithDebInfo ^
+    @REM -DBABYLON_NATIVE_BUILD_APPS=Off ^
+    @REM -DCMAKE_INSTALL_PREFIX=.\.install\x64\Release
+@REM if  %ERRORLEVEL% NEQ 0 (goto error)
+@REM cmake --build .\.build\Win32_x64_Napi-c-external_D3D11\Release --config RelWithDebInfo --parallel 20
+@REM if  %ERRORLEVEL% NEQ 0 (goto error)
+@REM cmake --build .\.build\Win32_x64_Napi-c-external_D3D11\Release --config RelWithDebInfo --target INSTALL
+@REM if  %ERRORLEVEL% NEQ 0 (goto error)
 
-
-
-@REM The UWP builds are not needed for Office development right now.
-@REM I haven't gotten the UWP builds to work on the command line, yet. There are issues with certificates that get resolved when opening the solution in Visual Studio.
-@REM There may be some clues on how to get the certificates working on the command line only here: https://stackoverflow.com/questions/48509114/how-to-create-a-working-trusted-and-or-self-signed-certificate-for-a-windows-10
-
-@REM if exist .\.build\UWP_arm64_Chakra_D3D11\Debug\ (rmdir /s /q .\.build\UWP_arm64_Chakra_D3D11\Debug)
-@REM cmake -B .\.build\UWP_arm64_Chakra_D3D11\Debug -A arm64 -D NAPI_JAVASCRIPT_ENGINE=Chakra -D GRAPHICS_API=D3D11 -D CMAKE_CONFIGURATION_TYPES=Debug -D CMAKE_INSTALL_PREFIX=.\.install\ARM64\Debug -D CMAKE_SYSTEM_NAME=WindowsStore -D CMAKE_SYSTEM_VERSION=10.0
-@REM cmake --build .\.build\UWP_arm64_Chakra_D3D11\Debug --config Debug --parallel 20
-@REM cmake --build .\.build\UWP_arm64_Chakra_D3D11\Debug --config Debug --target INSTALL
-
-@REM if exist .\.build\UWP_arm64_Chakra_D3D11\Release\ (rmdir /s /q .\.build\UWP_arm64_Chakra_D3D11\Release)
-@REM cmake -B .\.build\UWP_arm64_Chakra_D3D11\Release -A arm64 -D NAPI_JAVASCRIPT_ENGINE=Chakra -D GRAPHICS_API=D3D11 -D CMAKE_CONFIGURATION_TYPES=RelWithDebInfo -D CMAKE_INSTALL_PREFIX=.\.install\ARM64\Release -D CMAKE_SYSTEM_NAME=WindowsStore -D CMAKE_SYSTEM_VERSION=10.0
-@REM cmake --build .\.build\UWP_arm64_Chakra_D3D11\Release --config RelWithDebInfo --parallel 20
-@REM cmake --build .\.build\UWP_arm64_Chakra_D3D11\Release --config RelWithDebInfo --target INSTALL
-
-
-
-@REM The arm64 builds are not needed for Office development right now.
-
-@REM if exist .\.build\Win32_arm64_Chakra_D3D11\Debug\ (rmdir /s /q .\.build\Win32_arm64_Chakra_D3D11\Debug)
-@REM cmake -B .\.build\Win32_arm64_Chakra_D3D11\Debug -A arm64 -D NAPI_JAVASCRIPT_ENGINE=Chakra -D GRAPHICS_API=D3D11 -D CMAKE_CONFIGURATION_TYPES=Debug -D CMAKE_INSTALL_PREFIX=.\.install\ARM64\Debug
-@REM cmake --build .\.build\Win32_arm64_Chakra_D3D11\Debug --config Debug --parallel 20
-@REM cmake --build .\.build\Win32_arm64_Chakra_D3D11\Debug --config Debug --target INSTALL
-
-@REM if exist .\.build\Win32_arm64_Chakra_D3D11\Release\ (rmdir /s /q .\.build\Win32_arm64_Chakra_D3D11\Release)
-@REM cmake -B .\.build\Win32_arm64_Chakra_D3D11\Release -A arm64 -D NAPI_JAVASCRIPT_ENGINE=Chakra -D GRAPHICS_API=D3D11 -D CMAKE_CONFIGURATION_TYPES=RelWithDebInfo -D CMAKE_INSTALL_PREFIX=.\.install\ARM64\Release
-@REM cmake --build .\.build\Win32_arm64_Chakra_D3D11\Release --config RelWithDebInfo --parallel 20
-@REM cmake --build .\.build\Win32_arm64_Chakra_D3D11\Release --config RelWithDebInfo --target INSTALL
-
-
-
-if exist .\.office\include\ (rmdir /s /q .\.office\include)
+if exist .\.office\include\ (
+    rmdir /s /q .\.office\include
+)
 robocopy .\.install\x64\Debug\include .\.office\include /E
 
-if exist .\.office\lib\x64\ (rmdir /s /q .\.office\lib\x64)
+if exist .\.office\lib\x64\ (
+    rmdir /s /q .\.office\lib\x64
+)
 robocopy .\.install\x64\Debug\lib .\.office\lib\x64\Debug /E
-robocopy .\.install\x64\Release\lib .\.office\lib\x64\Release /E
+@REM robocopy .\.install\x64\Release\lib .\.office\lib\x64\Release /E
 
+del .\.office\*.js
+copy .\Apps\node_modules\babylonjs\babylon.max.js .\.office\
+copy .\Apps\node_modules\babylonjs-loaders\babylonjs.loaders.js .\.office\
+copy .\Apps\Playground\Scripts\experience.js .\.office\
 
+popd
+goto end
 
+:error
+popd
+@echo;
+@echo Error
+
+:end
 @echo;
 pause
